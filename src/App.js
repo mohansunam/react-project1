3ee11294da40c47043b2c45366ad36a1909b5a65
mohan <mohansunam234@gmail.com>
2024-07-06 17:10:37 +0545

    react-project2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import './pages/Home'
-import Home from './pages/Home'
-import MovieCard from './components/MovieCard'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
-      <Home/>
-  
+      <Header />
     </div>
   )
 }
